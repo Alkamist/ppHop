2ae4c988b31e1744f66ea46d962be2a93033e8cd
Corey Lehman <coreylehman@me.com>
2020-01-03 20:45:06 -0500

    Improved movement and added a test level.

--- Pp.gd
+++ Pp.gd
@@ -3,7 +3,7 @@ class_name Pp
 
 export var maximum_collisions_per_frame := 4
 export var controlling_player := 0
-export var friction := 20.0
+export var friction := 60.0
 export var bounciness := 0.8
 export var maximum_fall_velocity := 1400.0
 export var minimum_bounce_normal := 250.0
@@ -16,6 +16,7 @@ var _ping := 0
 var _target_transform := Transform2D()
 var _should_jump := false
 var _jump_direction := Vector2.ZERO
+var _jump_power := 0.0
 var _time := 0.0
 var _just_jumped := false
 var _friction_is_suspended := false
@@ -49,11 +50,16 @@ master func _launch_master(new_velocity):
 
 func _jump_if_needed(delta):
 	if _should_jump and _is_on_ground and _time_since_jump >= _jump_cooldown:
-		var jump_vector = _jump_direction
-		jump_vector = jump_vector.clamped(200.0)
-		jump_vector = pow(200.0, 0.3) * jump_vector / pow(jump_vector.length(), 0.3)
-		jump_vector.y = min(jump_vector.y, -50.0)
-		velocity = jump_vector * 5.0
+		var jump_vector = _jump_direction / 300.0
+		jump_vector = jump_vector.clamped(1.0)
+		var length = jump_vector.length()
+		if length > 0.0:
+			jump_vector.y += 0.25
+			jump_vector.y = min(jump_vector.y, 0.0)
+			jump_vector *= 1.0 / pow(length, 0.5)
+			jump_vector.y -= 0.25
+			jump_vector *= 300.0
+		velocity = jump_vector * 3.3
 		_just_jumped = true
 		_time_since_jump = 0.0
 		suspend_friction()
@@ -125,6 +131,19 @@ func update_state(delta):
 	if _cant_launch_pp_timer > 0.15:
 		_cant_launch_pp = false
 
+#func _process(delta):
+#	update()
+
+#func _draw():
+#	if is_controlling_player():
+#		var jump_vector = _jump_direction / 300.0
+#		jump_vector = jump_vector.clamped(1.0)
+#		var length = jump_vector.length()
+#		if length > 0.0:
+#			jump_vector.y = min(jump_vector.y, -0.25)
+#			jump_vector *= 300.0
+#			draw_line(Vector2(0,0), jump_vector, Color(255, 0, 0), 1)
+
 func _physics_process(delta):
 	if not is_server():
 		rpc_unreliable_id(1, "_ping_server", get_tree().get_network_unique_id(), _tick)
@@ -134,13 +153,18 @@ func _physics_process(delta):
 		_should_jump = Input.is_action_pressed("jump")
 
 	if is_network_master():
-		update_state(delta)
+		if Input.is_action_pressed("down"):
+			position = position.linear_interpolate(get_global_mouse_position(), 0.1)
+			velocity = Vector2.ZERO
+			velocity_before_collision = Vector2.ZERO
+		else:
+			update_state(delta)
 		rpc_unreliable("_set_sprite_direction", _jump_direction)
 		rpc_unreliable("_update_clients", transform, velocity, velocity_before_collision)
 	else:
 		var distance = position.distance_to(_target_transform.origin)
 		if distance > 0.0:
-			position = position.linear_interpolate(_target_transform.origin, 0.44)
+			position = position.linear_interpolate(_target_transform.origin, 0.5)
 		else:
 			position = _target_transform.origin
 
