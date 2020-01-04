45da13153b0d8f3c91b8cfc28bbda8404de88e95
Corey Lehman <coreylehman@me.com>
2020-01-04 12:31:13 -0500

    Messing around with a different movement scheme.

--- Pp.gd
+++ Pp.gd
@@ -11,21 +11,18 @@ export var gravity := 1500.0
 export var velocity := Vector2.ZERO
 export var velocity_before_collision := Vector2.ZERO
 
-var _tick := 0
-var _ping := 0
+var _movement_direction := 0
 var _target_transform := Transform2D()
-var _should_jump := false
-var _jump_direction := Vector2.ZERO
-var _jump_power := 0.0
-var _time := 0.0
-var _just_jumped := false
-var _friction_is_suspended := false
-var _time_since_jump := 0.0
-var _jump_cooldown := 0.1
-var _suspend_friction_timer := 0.0
 var _is_on_ground := false
+var _should_jump := false
+var _is_charging_jump := false
+var _friction_is_suspended := false
 var _cant_launch_pp := false
-var _cant_launch_pp_timer := 0.0
+
+var _time := 0.0
+var _launched_pp_time := 0.0
+var _suspend_friction_time := 0.0
+var _jump_time := 0.0
 
 func initialize():
 	if is_controlling_player():
@@ -39,7 +36,7 @@ func is_server():
 
 func suspend_friction():
 	_friction_is_suspended = true
-	_suspend_friction_timer = 0.0
+	_suspend_friction_time = _time
 
 func launch(new_velocity):
 	rpc_unreliable("_launch_master", new_velocity)
@@ -48,22 +45,21 @@ master func _launch_master(new_velocity):
 	suspend_friction()
 	velocity = new_velocity
 
-func _jump_if_needed(delta):
-	if _should_jump and _is_on_ground and _time_since_jump >= _jump_cooldown:
-		var jump_vector = _jump_direction / 300.0
-		jump_vector = jump_vector.clamped(1.0)
-		var length = jump_vector.length()
-		if length > 0.0:
-			jump_vector.y += 0.25
-			jump_vector.y = min(jump_vector.y, 0.0)
-			jump_vector *= 1.0 / pow(length, 0.5)
-			jump_vector.y -= 0.25
-			jump_vector *= 300.0
-		velocity = jump_vector * 3.3
-		_just_jumped = true
-		_time_since_jump = 0.0
-		suspend_friction()
-		play_sound("PPJump")
+func _handle_jumping(delta):
+	if _should_jump:
+		if _is_on_ground and not _is_charging_jump:
+			_is_charging_jump = true
+			_jump_time = _time
+	if not _is_on_ground:
+		_is_charging_jump = false
+	var jump_power = (_time - _jump_time)
+	if not _should_jump or jump_power > 0.5:
+		if _is_on_ground and _is_charging_jump:
+			velocity.x = _movement_direction * 500.0
+			velocity.y = -2000.0 * clamp(jump_power, 0.1, 0.5)
+			suspend_friction()
+			play_sound("PPJump")
+			_is_charging_jump = false
 
 func _collision_is_ground(collision):
 	return abs(collision.normal.angle_to(Vector2.UP)) < 0.78
@@ -86,7 +82,7 @@ func _handle_movement_and_collisions(delta):
 	var collision_count = 0
 	while collision and collision_count < maximum_collisions_per_frame and collision.remainder.length() > 0.0:
 		var collider = collision.collider
-		
+
 		# Slide on the floor or a pp head.
 		if _collision_is_ground(collision):
 			velocity = velocity.slide(collision.normal)
@@ -99,7 +95,7 @@ func _handle_movement_and_collisions(delta):
 			if not _cant_launch_pp:
 				collider.launch(velocity_before_collision * bounciness)
 				_cant_launch_pp = true
-				_cant_launch_pp_timer = 0.0
+				_launched_pp_time = _time
 
 		# Bounce off walls.
 		else:
@@ -114,43 +110,30 @@ func _handle_movement_and_collisions(delta):
 
 		collision_count += 1
 
-remotesync func _set_sprite_direction(jump_direction):
-	get_node("../Smoothing2D/Sprite").set_flip_h(jump_direction.x < 0)
-
 func update_state(delta):
 	_check_if_on_ground(delta)
-	_jump_if_needed(delta)
+	_handle_jumping(delta)
 	_apply_gravity(delta)
-	_apply_friction(delta)
+	#_apply_friction(delta)
+	var new_x_velocity = velocity.x + _movement_direction * 25.0
+	if abs(new_x_velocity) < 200.0:
+		velocity.x = new_x_velocity
 	_handle_movement_and_collisions(delta)
-	_time_since_jump += delta
-	_suspend_friction_timer += delta
-	_cant_launch_pp_timer += delta
-	if _suspend_friction_timer > 0.15:
+	_time += delta
+	if _time - _suspend_friction_time > 0.15:
 		_friction_is_suspended = false
-	if _cant_launch_pp_timer > 0.15:
+	if _time - _launched_pp_time > 0.15:
 		_cant_launch_pp = false
 
-#func _process(delta):
-#	update()
-
-#func _draw():
-#	if is_controlling_player():
-#		var jump_vector = _jump_direction / 300.0
-#		jump_vector = jump_vector.clamped(1.0)
-#		var length = jump_vector.length()
-#		if length > 0.0:
-#			jump_vector.y = min(jump_vector.y, -0.25)
-#			jump_vector *= 300.0
-#			draw_line(Vector2(0,0), jump_vector, Color(255, 0, 0), 1)
+remotesync func _set_sprite_facing_right(value):
+	get_node("../Smoothing2D/Sprite").set_flip_h(not value)
 
 func _physics_process(delta):
-	if not is_server():
-		rpc_unreliable_id(1, "_ping_server", get_tree().get_network_unique_id(), _tick)
-
 	if is_controlling_player():
-		_jump_direction = get_global_mouse_position() - global_position
 		_should_jump = Input.is_action_pressed("jump")
+		var left = -1 if Input.is_action_pressed("left") else 0
+		var right = 1 if Input.is_action_pressed("right") else 0
+		_movement_direction = left + right
 
 	if is_network_master():
 		if Input.is_action_pressed("down"):
@@ -159,7 +142,10 @@ func _physics_process(delta):
 			velocity_before_collision = Vector2.ZERO
 		else:
 			update_state(delta)
-		rpc_unreliable("_set_sprite_direction", _jump_direction)
+		if _movement_direction == -1:
+			rpc_unreliable("_set_sprite_facing_right", false)
+		elif _movement_direction == 1:
+			rpc_unreliable("_set_sprite_facing_right", true)
 		rpc_unreliable("_update_clients", transform, velocity, velocity_before_collision)
 	else:
 		var distance = position.distance_to(_target_transform.origin)
@@ -168,20 +154,11 @@ func _physics_process(delta):
 		else:
 			position = _target_transform.origin
 
-	_time += delta
-	_tick += 1
-
 func play_sound(sound_name):
 	rpc_unreliable("_play_networked_sound", sound_name)
 
 remotesync func _play_networked_sound(sound_name):
 	SFX.play(sound_name, self)
-
-remote func _ping_server(network_id, tick):
-	rpc_unreliable_id(network_id, "_update_ping", tick)
-
-remote func _update_ping(tick):
-	_ping = _tick - tick
 
 remote func _update_clients(new_transform, new_velocity, new_velocity_before_collision):
 	velocity = new_velocity
