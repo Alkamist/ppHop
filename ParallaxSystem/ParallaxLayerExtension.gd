bc22fb4566a1772439c71a371d4350e64f108ea4
Corey Lehman <coreylehman@me.com>
2020-02-17 16:46:46 -0500

    Made some changes to the marsh and cosmic horror place.

--- ParallaxSystem/ParallaxLayerExtension.gd
+++ ParallaxSystem/ParallaxLayerExtension.gd
@@ -23,8 +23,8 @@ func _add_depth_layer(at_depth):
 	layer_sprite.scale *= scale_value
 	layer_sprite.material = layer_sprite.material.duplicate()
 	layer_sprite.material.set_shader_param("fade_color", distance_tint)
-	layer_sprite.material.set_shader_param("tint_strength", at_depth * tinting / depth)
-	#layer_sprite.position.x += new_scale.x * x_variation * (randf() * 2.0 - 1.0)
+	#layer_sprite.material.set_shader_param("tint_strength", at_depth * tinting / depth)
+	layer_sprite.material.set_shader_param("tint_strength", tinting * pow(float(at_depth) / float(depth), 0.8))
 	layer_sprite.position.x += round(x_variation * randf() * layer_sprite.texture.get_width())
 	layer_sprite.region_rect.size.x /= scale_value
 	new_layer.add_child(layer_sprite)
